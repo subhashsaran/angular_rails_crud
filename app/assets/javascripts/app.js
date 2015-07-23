de9761d9aaad1f4a8ed29cc5541f54a906c7c148
Peeyush Singla <peeyush@fizzysoftware.com>
2015-07-23 14:13:21 +0530

    everything working fine till added templates

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,18 @@
-myApp = angular.module('myApp',[])
+myApp = angular.module('myApp',['ngRoute', 'templates'])
 
 $(document).on('ready page:load', function(event) {
   angular.bootstrap(document, ['myApp'])
 });
+
+// myApp.config(['$routeProvider',
+//   function($routeProvider) {
+//     $routeProvider.
+//       when('/', {
+//         templateUrl: 'index.html',
+//         controller: 'SomeController'
+//       }).
+//       when('/recipes/new', {
+//         templateUrl: 'new.html',
+//         controller: 'SomeOtherController'
+//       })
+//   }]);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-route
+//= require angular-rails-templates
 //= require angular-resource
 //= require turbolinks
 //= require_tree .
